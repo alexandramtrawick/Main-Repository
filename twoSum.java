f9c15cf8b57e1b1d5aacaf52d8adc6078f8b225b
Alexandra <alexandramtrawick@gmail.com>
2024-08-26 14:12:00 -0500

    pyramid.js

--- twoSum.java
+++ twoSum.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class twoSum {
+import java.lang.reflect.Array;
+
+public class twoSum {
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            int[] ans = new int[2];
+            for(int i = 0; i<nums.length; i++){
+                for(int j = 0; j<nums.length; j++){
+                    //potential problem, number checking if it plus itself = target
+                    if (nums[i]+nums[j] == target){
+                        ans[0] = i;
+                        ans[1] = j;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
 }
